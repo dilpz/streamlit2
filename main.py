c980a4f980fb0572eb2b0e7245084eb268be9edd
dilpz <37154665+dilpz@users.noreply.github.com>
2023-12-04 16:37:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import streamlit as st
 import pandas as pd
 import os
 
-# Function to save data to Excel
-def save_to_excel(data, filename="rsvp_data.xlsx"):
+# Function to save data to CSV
+def save_to_csv(data, filename="rsvp.csv"):
     if not os.path.isfile(filename):
-        # Create a new Excel file and add data
+        # Create a new CSV file and add data
         df = pd.DataFrame(data, columns=["Name", "Attending", "Number of Guests"])
-        df.to_excel(filename, index=False)
+        df.to_csv(filename, index=False)
     else:
-        # Append data to existing Excel file
+        # Append data to existing CSV file
         df = pd.DataFrame(data, columns=["Name", "Attending", "Number of Guests"])
-        with pd.ExcelWriter(filename, mode='a', engine='openpyxl') as writer:
-            df.to_excel(writer, sheet_name="RSVP", index=False)
+        df.to_csv(filename, mode='a', index=False, header=False)
 
 # Streamlit UI
 st.title("Wedding RSVP")
@@ -26,13 +25,13 @@ num_guests = st.number_input("Number of guests", min_value=0)
 # Button to submit response
 if st.button("Submit"):
     data = [[name, attending, num_guests]]
-    save_to_excel(data)
+    save_to_csv(data)
     st.success("Thank you for your response!")
 
-# Displaying the Excel file content (optional)
+# Displaying the CSV file content (optional)
 if st.checkbox("Show RSVP data"):
-    if os.path.isfile("rsvp_data.xlsx"):
-        df = pd.read_excel("rsvp_data.xlsx")
+    if os.path.isfile("rsvp.csv"):
+        df = pd.read_csv("rsvp.csv")
         st.write(df)
     else:
         st.write("No data available yet.")
